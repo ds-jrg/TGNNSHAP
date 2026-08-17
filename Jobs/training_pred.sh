7ad115b765b09bfb8d7b210c249bf67e51efe8a2
sussekl <sussekl@mail.uni-paderborn.de>
2026-08-17 09:55:28 +0200

    Removed review dataset

--- Jobs/training_pred.sh
+++ Jobs/training_pred.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 #SBATCH --mail-type END,FAIL
 #SBATCH --mail-user sussekl@mail.uni-paderborn.de
-#SBATCH -t 10:00:00
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem-per-cpu=2G
+#SBATCH -t 5:00:00
 #SBATCH --gres=gpu:a100:1
 #SBATCH -J "Shapley_Experiment_%a"
 #SBATCH -p gpu
 #SBATCH -A hpc-prf-wiki
-#SBATCH --array=0-2
+#SBATCH --array=0-7
 
 module load lang/Python/3.9.5-GCCcore-10.3.0
 module load lib/libffi/3.4.5-GCCcore-13.3.0
 module load system/CUDA/12.4.1
 source venv/bin/activate
 
-options=( "MOOC" "Reddit" "Wikipedia" )
+options=( "Flights" "MOOC" "Reddit" "UNtrade" "UNvote" "USLegis" "Wikipedia" )
 
-srun --ntasks=1 --cpus-per-task=16 --gres=gpu:a100:1 python -m Evaluation.Training.prediction -d ${options[$SLURM_ARRAY_TASK_ID]}
+python -u -m Evaluation.Training.prediction -d ${options[$SLURM_ARRAY_TASK_ID]}
